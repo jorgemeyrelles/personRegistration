bbbd6c8839de45aeb3d334c036be90e79bb8d113
jorgemeyrelles <jotaengpuc@gmail.com>
2025-06-25 18:28:17 -0300

    feat: it has been created exceptions

---
+++ apiPersonRegistration/src/main/java/br/com/personreg/domain/exception/DuplicateResourceException.java
@@ -0,0 +1,14 @@
+package br.com.personreg.domain.exception;
+
+public class DuplicateResourceException extends RuntimeException {
+
+	public DuplicateResourceException(String message) {
+		super(message);
+	}
+
+	public DuplicateResourceException(String resourceName, String fieldName,
+			Object fieldValue) {
+		super(String.format("%s already exists with %s: '%s'", resourceName,
+				fieldName, fieldValue));
+	}
+}
