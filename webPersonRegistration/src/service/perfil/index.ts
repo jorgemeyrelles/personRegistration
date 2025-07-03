76342d671a0b8fc4dfa4c1d79fe815c9abc70d4b
Jorge Meyrelles Jr. <jotaengpuc@gmail.com>
2025-07-03 07:12:38 -0300

    Update services index.ts

--- webPersonRegistration/src/service/perfil/index.ts
+++ webPersonRegistration/src/service/perfil/index.ts
@@ -1,4 +1,4 @@
-import { API_BASE_URL } from "../../config/api";
+// import { API_BASE_URL } from "../../config/api";
 
 import type {
   PerfilCriar,
@@ -6,8 +6,7 @@ import type {
   PerfilResponse,
 } from "../../types/perfil";
 
-// const API_BASE_URL =
-//   import.meta.env.VITE_API_BASE_URL || "http://localhost:8081";
+ const API_BASE_URL = "http://localhost:8081";
 
 // Serviço para criar perfil
 export const criarPerfil = async (
