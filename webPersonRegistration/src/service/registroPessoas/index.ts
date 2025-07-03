e8856d3a1c9386a512008a895a88dd5da2735005
Jorge Meyrelles Jr. <jotaengpuc@gmail.com>
2025-07-03 07:11:55 -0300

    Update services index.ts

--- webPersonRegistration/src/service/registroPessoas/index.ts
+++ webPersonRegistration/src/service/registroPessoas/index.ts
@@ -1,4 +1,4 @@
-import { API_BASE_URL } from "../../config/api";
+// import { API_BASE_URL } from "../../config/api";
 
 import type {
   RegistroPessoaRequest,
@@ -8,8 +8,8 @@ import type {
   CPFExisteResponse,
 } from "../../types/registroPessoasTypes";
 
-// const API_BASE_URL =
-//   import.meta.env.VITE_API_BASE_URL || "http://localhost:8081";
+ const API_BASE_URL =
+   import.meta.env.VITE_API_BASE_URL || "http://localhost:8081";
 
 // Serviço para criar registro de pessoa
 export const criarRegistroPessoa = async (
